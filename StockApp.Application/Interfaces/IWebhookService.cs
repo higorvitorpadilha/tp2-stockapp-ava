df7665ca15e4d8f48811b9f6b14cc19492c8bda4
Higor <higorvitorpadilha@gmail.com>
2024-11-20 14:58:19 -0300

    código webhook feito

---
+++ StockApp.Application/Interfaces/IWebhookService.cs
@@ -0,0 +1,8 @@
+﻿namespace StockApp.Application.Interfaces
+{
+    public interface IWebhookService
+    {
+        Task SendWebhookAsync(string url, object payload);
+        void RegisterWebhook(string url, string eventType);
+    }
+}
